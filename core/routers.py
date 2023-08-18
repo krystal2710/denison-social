1f2316e5cf645ad7587f5d9731a1411dd698f5b7
Krystal Ly <ly_k1@denison.edu>
2023-08-18 15:09:03 -0500

    Delete py cache files + Add comment model

--- core/routers.py
+++ core/routers.py
@@ -1,6 +1,7 @@
-from rest_framework import routers
+from rest_framework_nested import routers
 from core.user.viewsets import UserViewSet
 from core.auth.viewsets import RegisterViewSet, LoginViewSet, RefreshViewSet
+from core.post.viewsets import PostViewSet
 
 router = routers.SimpleRouter()
 # ##################################################################### #
@@ -14,6 +15,12 @@ router.register(r'user', UserViewSet, basename='user')
 router.register(r'auth/register', RegisterViewSet, basename='auth-register')
 router.register(r'auth/login', LoginViewSet, basename='auth-login')
 router.register(r'auth/refresh', RefreshViewSet, basename='auth-refresh')
+
+# ##################################################################### #
+# ################### POST                       ###################### #
+# ##################################################################### #
+router.register(r'post', PostViewSet, basename='post')
+posts_router = routers.NestedSimpleRouter(router, r'post', lookup='post')
 
 urlpatterns = [
    *router.urls,
