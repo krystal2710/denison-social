d7940f1b498ac43e7c61964891ff4bffedcaf782
Krystal Ly <ly_k1@denison.edu>
2023-09-01 16:34:39 -0400

    Update

--- docker-ec2-deploy.sh
+++ docker-ec2-deploy.sh
@@ -9,7 +9,7 @@ echo -e ${ACTION_COLOR} Checking if we are on the target branch
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
 if [ "$BRANCH" != ${TARGET} ]
 then
-	  exit 0
+	exit 0
 fi
 
 # Checking if the repository is up to date.
@@ -20,23 +20,28 @@ UPSTREAM_HASH=$(git rev-parse ${TARGET}@{upstream})
 
 if [ "$HEAD_HASH" == "$UPSTREAM_HASH" ]
 then
-	  echo -e "${FINISHED}"The current branch is up to date with origin/${TARGET}."${NO_COLOR}"
-	    exit 0
+	echo -e "${FINISHED}"The current branch is up to date with origin/${TARGET}."${NO_COLOR}"
+fi
+
+if [ $( docker ps -a | grep denison_social_api | wc -l ) -gt 0 ] && [ $( docker ps -a | grep denison_social_db | wc -l ) -gt 0 ] && [ $( docker ps -a | grep denison_social_web | wc -l ) -gt 0 ] && [ $( docker ps -a | grep denison-social_redis_1 | wc -l ) -gt 0 ] ; then
+  	echo "all resources exists"
+else
+	echo "missing resources"
+	echo “Running docker-compose again”
+	docker-compose up -d --build
+	echo -e "Succesfully run docker-compose"
 fi
 
 # If there are new changes, we pull these changes.
-
-git pull origin main;
-
-echo -e "Succesfully pull new changes"
-
-
-# We can now build and start the containers
-
-docker-compose up -d --build
+if [ "$HEAD_HASH" == "$UPSTREAM_HASH" ]
+then
+	  git pull origin main;
+	  # We can now build and start the containers
+	  echo -e "Succesfully pull new changes"
+	  docker-compose up -d --build
+	  echo -e "Succesfully run docker-compose"
+fi
 
 docker container ls
-
-echo -e "Succesfully run docker-compose"
 
 exit 0;
